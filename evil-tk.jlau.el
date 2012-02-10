2d765386c8fba655b538c31ff99c6e9582192740
Jeffrey Lau <who.else.at.jlau.tk>
2012-02-11 01:56:40 +0800

    evil: use the backtick macro (` ...) to work around the closure-less elisp & non-working lexical-let of `cl'

--- evil-tk.jlau.el
+++ evil-tk.jlau.el
@@ -114,21 +114,20 @@
   )
 
 ;; Remap org-mode meta keys for convenience
-;;;###autoload
+;; Yeah... need to use backticks for evil's `evil-delay'...
+;;###autoload
 (mapc (lambda (state)
-         (lexical-let ((state state))
-           (evil-declare-key state org-mode-map
-             (kbd "C-t") 'org-todo
-             (kbd "M-l") 'org-metaright
-             (kbd "M-h") 'org-metaleft
-             (kbd "M-k") 'org-metaup
-             (kbd "M-j") 'org-metadown
-             (kbd "M-L") 'org-shiftmetaright
-             (kbd "M-H") 'org-shiftmetaleft
-             (kbd "M-K") 'org-shiftmetaup
-             (kbd "M-J") 'org-shiftmetadown)
-           )
-         )
-      '(normal insert))
+          (eval `(evil-declare-key ',state org-mode-map
+                   (kbd "C-t") 'org-todo
+                   (kbd "M-l") 'org-metaright
+                   (kbd "M-h") 'org-metaleft
+                   (kbd "M-k") 'org-metaup
+                   (kbd "M-j") 'org-metadown
+                   (kbd "M-L") 'org-shiftmetaright
+                   (kbd "M-H") 'org-shiftmetaleft
+                   (kbd "M-K") 'org-shiftmetaup
+                   (kbd "M-J") 'org-shiftmetadown)
+                ))
+        '(normal insert))
 
 (provide 'evil-tk.jlau)
