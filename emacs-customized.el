bcf9f0d78ff8f47e61adbcf519c654087f1560b6
Jeffrey Lau <who.else.at.jlau.tk>
2012-08-21 21:18:53 +0800

    customize: Remove 'display-time-mode
    XXX: Didn't expect that!  What caused it to change?

--- emacs-customized.el
+++ emacs-customized.el
@@ -1,12 +1,11 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(backup-directory-alist (quote (("." . "~/.emacs.d/tmp"))))
  '(blink-cursor-mode nil)
  '(column-number-mode t)
- '(display-time-mode nil)
  '(ecb-options-version "2.40")
  '(evil-shift-width 2)
  '(evil-want-C-u-scroll t)
