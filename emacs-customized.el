77be6621c89900a42dca9f02b6a5e3a958fd44fe
Jeffrey Lau <who.else.at.jlau.tk>
2012-08-21 21:17:53 +0800

    customize: Increase default font size to 14
    ... and change the font to 'DejaVu Sans Mono'?!  I didn't expect that!

--- emacs-customized.el
+++ emacs-customized.el
@@ -27,8 +27,8 @@
  '(show-trailing-whitespace t)
  '(x-stretch-cursor t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "monospace")))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 143 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
