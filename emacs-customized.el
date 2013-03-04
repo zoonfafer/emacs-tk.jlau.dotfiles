96ddcb82a537ab7fc984c7588d616bde9d4afc4c
Jeffrey Lau <who.else.at.jlau.tk>
2013-03-04 22:02:53 +0800

    customize: Use system font?!
    What is a system font?

--- emacs-customized.el
+++ emacs-customized.el
@@ -9,6 +9,7 @@
  '(ecb-options-version "2.40")
  '(evil-shift-width 2)
  '(evil-want-C-u-scroll t)
+ '(font-use-system-font t)
  '(ido-enable-flex-matching t)
  '(ido-enable-regexp nil)
  '(ido-enable-tramp-completion nil)
@@ -30,4 +31,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 143 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
